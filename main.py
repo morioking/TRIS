06b124b87747e913d53ac6204bb6c617d5fa97f1
morioking <morioking@gmail.com>
2015-07-30 08:50:21 +0900

    CHANGE
    main.py 列のキーを宣言

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ import re
 ROW = 24
 COLOUMN = 20
 
+NUM = 0
+TITLE = 1
+ARTIST = 2
+TIME = 3
+BPM = 4
+TRACK = 5
+RELEASE = 6
+LABEL = 7
+GENRE = 8
+KEY_TEXT = 9
+KEY = 10
+COMMENT = 11
+LYRICS = 12
+RATING = 13
+FILE = 14
+ANALYZED = 15
+REMIXER = 16
+PRODUCER = 17
+RELEASE_DATE = 18
+BITRATE = 19
+COMMENT2 = 20
+PLAY_COUNT = 21
+LAST_PLAYED = 22
+IMPORT_DATE = 23
+
+
 class MyClass:
 	def __init__(self):
 		self.name = "hoge"
@@ -30,7 +56,8 @@ print argvs
 print argc
 print argvs[1]
 
-print 'The content of %s ....' % argvs[1]
+#print 'The content of %s ....' % argvs[1]
+
 f = open(argvs[1])
 line = f.readline()
 pattern = r"<tr>"
@@ -44,12 +71,14 @@ while line:
 	if re.match(".*<th>", line):
 		if i < 10:
 			table[i][j] = line
-		print line
-		print i
+		#print line
+		#print i
 		j += 1
 	if re.match("<tr>", line):
 		i += 1
 		#print line
 	line = f.readline()
 print table
+#print table[1]
+#print table[0][NUM]
 f.close
